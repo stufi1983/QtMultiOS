4c54d1bce96316aec2b1056cc8dd9fee9c032fff
stufi1983 <stufi1983@gmail.com>
2025-04-13 17:58:15 +0700

    linux fix

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QIcon>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    
+    QIcon appIcon(":/resources/icon.png");
+    if (appIcon.isNull()) {
+        qDebug() << "Failed to load application icon!";
+    } else {
+        qDebug() << "Application icon loaded successfully.";
+    }
+    a.setWindowIcon(appIcon);
+
     MainWindow w;
-    w.setWindowIcon(QIcon("resources/icon.png")); 
+    QIcon windowIcon(":/resources/icon.png");
+    if (windowIcon.isNull()) {
+        qDebug() << "Failed to load window icon!";
+    } else {
+        qDebug() << "Window icon loaded successfully.";
+    }
+    w.setWindowIcon(windowIcon);
     w.setWindowTitle("Qt Window with Icon");
     w.show();
     return a.exec();
